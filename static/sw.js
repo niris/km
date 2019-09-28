b9955941fc31380431ae95821679c1625716d45a
niris <niris@users.noreply.github.com>
2019-09-28 16:26:29 +0000

    Automated deployment: Sat Sep 28 16:26:29 UTC 2019 f08c578380bcfee0275bbd0df6eefc7559f972c1

--- static/sw.js
+++ static/sw.js
@@ -3,7 +3,7 @@ const cacheName = `km-${version}`;
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll([//(grep -o 'http[^")'"']*" static/index.html ; ls static/*) | sed -E "s:(.*):'\1',:"
+      return cache.addAll([//(grep -o 'http[^")'"']*" static/index.html ; ls static) | sed -E "s:(.*):'\1',:"
         `/`,
 'https://unpkg.com/chota@0.6.2/dist/chota.min.css',
 'https://d3js.org/d3.v5.min.js',
@@ -11,20 +11,20 @@ self.addEventListener('install', e => {
 'https://unpkg.com/vue@2.6.10/dist/vue.min.js',
 'https://unpkg.com/vue-router@3.1.3/dist/vue-router.min.js',
 'https://unpkg.com/vue-authfetch@0.0.4/index.js',
-'static/graph.js',
-'static/index.html',
-'static/KmAvatar.js',
-'static/KmField.js',
-'static/KmFieldList.js',
-'static/KmGraph.js',
-'static/logo.png',
-'static/logo.svg',
-'static/manifest.json',
-'static/PageActivity.js',
-'static/PageHome.js',
-'static/PageSign.js',
-'static/PageUser.js',
-'static/sw.js',
+'graph.js',
+'index.html',
+'KmAvatar.js',
+'KmField.js',
+'KmFieldList.js',
+'KmGraph.js',
+'logo.png',
+'logo.svg',
+'manifest.json',
+'PageActivity.js',
+'PageHome.js',
+'PageSign.js',
+'PageUser.js',
+'sw.js',
       ])
           .then(() => self.skipWaiting());
     })
